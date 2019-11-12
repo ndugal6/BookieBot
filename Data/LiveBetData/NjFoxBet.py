2d526315510a5449789a3d6e7c79f10d1e71ef9f
Nick <18618975+ndugal6@users.noreply.github.com>
2019-11-12 15:24:24 -0500

    added both main apis for NJfox bet data

--- Data/LiveBetData/NjFoxBet.py
+++ Data/LiveBetData/NjFoxBet.py
@@ -7,7 +7,7 @@ def getNJFOX_Lines():
     host = 'http://sports.nj.foxbet.com'
     path = '/sportsbook/v1/api/getCompetitionEvents'
     params = {
-        'competitionId': 4700094,  # id of comp from other api call
+        'competitionId': get_nba_id(),  # id of comp from other api call
         'includeOutrights': 'false',  # unknown meaning
         'channelId': 14,  # unknown meaning
         'locale': 'en-us'
@@ -24,3 +24,38 @@ def getEventNames():
     for event in events:
         names.append(event['name'])
     return names
+
+
+def get_nba_id():
+    response = get_sports_tree()
+    for category in response.json()['categories']:
+        if category['name'].__contains__('USA'):
+            for comp in category['competition']:
+                if comp['name'].__contains__('USA - NBA'):
+                    return comp['id']
+    return None
+
+
+def get_sports_tree():
+    path = '/sportsbook/v1/api/getSportTree'
+    host = 'http://sports.nj.foxbet.com'
+    params = {
+        'sport': 'BASKETBALL',  # id of comp from other api call
+        'includeOutrights': 'false',  # unknown meaning
+        'includeEvents': 'false',  # unknown meaning
+        'channelId': 14,  # unknown meaning
+        'locale': 'en-us'
+    }
+    url = host + path + '?' + urllib.parse.urlencode(params)
+    response = requests.get(url)
+    print(response.status_code)
+    return response
+
+
+
+def main():
+    print(get_nba_id())
+    print(getEventNames())
+
+if __name__ == '__main__':
+    main()
